08c0f231179db368a1a8d02c6722e4dcdf676494
ijun17 <ijun17@naver.com>
2025-01-02 15:55:35 +0900

    feat: svgr로 헤더 로고 및 메뉴 아이콘  svg로 변경

--- src/components/MainLayout/Header.tsx
+++ src/components/MainLayout/Header.tsx
@@ -1,23 +1,13 @@
+import Logo from "@/assets/logo.svg?react";
+import HamburgerMenuIcon from "@/assets/hamburger_menu_icon.svg?react";
+
 export const Header = () => {
   return (
     <header className="flex-1 flex justify-center p-4 bg-white">
       <div className="flex justify-between items-center max-w-screen-lg w-full">
-        <h1 className="text-xl font-bold">iMMate</h1>
+        <Logo />
         <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 5.25h16.5m-16.5 6h16.5m-16.5 6h16.5"
-            />
-          </svg>
+          <HamburgerMenuIcon />
         </button>
       </div>
     </header>
