971bbc711c0f38d34710f4f521abb282e6c1aa53
ijun17 <ijun17@naver.com>
2025-01-07 03:51:24 +0900

    feat: 메인 페이지 및 유저 페이지 api 연결

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,18 +1,45 @@
+import {
+  fetchMyCrews,
+  fetchRecommendedCrews,
+  fetchTopRankingCrews,
+} from "@/api/crew";
+import CrewCreationModal from "@/components/CrewCreationModal";
 import { MainLayout } from "@/components/MainLayout";
+import { Crew } from "@/types/api";
+import { useQueries } from "@tanstack/react-query";
+import { Link } from "@tanstack/react-router";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
-type Crew = {
-  name: string;
-  value: string;
-  change: string;
-};
+// type Crew = {
+//   name: string;
+//   value: string;
+//   change: string;
+// };
 
 export const MainPage = () => {
+  const results = useQueries({
+    queries: [
+      { queryKey: ["my_crew"], queryFn: fetchMyCrews, staleTime: Infinity },
+      {
+        queryKey: ["recommend_crew"],
+        queryFn: fetchRecommendedCrews,
+        staleTime: Infinity,
+      },
+      {
+        queryKey: ["crew_ranking"],
+        queryFn: fetchTopRankingCrews,
+        staleTime: Infinity,
+      },
+    ],
+  });
+
   return (
     <MainLayout>
       <ProfitCard />
-      <CrewSection title="나의 크루" actionText="+" />
-      <CrewSection title="추천 크루" />
-      <CrewSection title="크루 랭킹" />
+      <CrewSection title="나의 크루" crews={results[0].data} actionText="+" />
+      <CrewSection title="추천 크루" crews={results[1].data} />
+      <CrewSection title="크루 랭킹" crews={results[2].data} />
     </MainLayout>
   );
 };
@@ -20,9 +47,9 @@ export const MainPage = () => {
 const ProfitCard = () => {
   return (
     <div className="bg-purple-50 rounded-lg p-4 shadow mb-6">
-      <h2 className="text-sm font-medium text-gray-700">내 공동 투자 수익률</h2>
+      <h2 className="text-sm font-medium text-gray-400">내 공동 투자 수익률</h2>
       <p className="text-2xl font-bold">$365.51</p>
-      <p className="text-sm text-gray-500">$365.51</p>
+      <p className="text-sm text-gray-500">+ $20.51</p>
       <div className="flex items-center mt-2">
         <div className="flex-1 bg-purple-200 h-1 rounded-full relative">
           <div
@@ -36,35 +63,39 @@ const ProfitCard = () => {
   );
 };
 
-const CrewSection = ({
-  title,
-  actionText,
-}: {
+const CrewSection = (props: {
   title: string;
+  crews?: Crew[];
   actionText?: string;
 }) => {
-  const crews: Crew[] = [
-    { name: "일론 머스크 크루", value: "365만원", change: "+3.12%" },
-    { name: "IT Tech Group 1", value: "365만원", change: "+3.12%" },
-    { name: "IT Tech Group 1", value: "365만원", change: "+3.12%" },
-    { name: "일론 머스크 크루", value: "365만원", change: "+3.12%" },
-    { name: "IT Tech Group 1", value: "365만원", change: "+3.12%" },
-    { name: "IT Tech Group 1", value: "365만원", change: "+3.12%" },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-lg font-semibold">{title}</h2>
-        {actionText && (
-          <button className="text-purple-500 text-2xl font-bold hover:text-purple-600">
-            {actionText}
-          </button>
+        <h2 className="text-lg font-semibold">{props.title}</h2>
+        {props.actionText && (
+          <>
+            <button
+              className="bg-purple-500 text-white p-1 text-sm rounded-md font-bold hover:bg-purple-600"
+              onClick={() => setIsOpen(true)}
+            >
+              크루 만들기
+            </button>
+            {createPortal(
+              <CrewCreationModal
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+              />,
+              document.body
+            )}
+          </>
         )}
       </div>
       <div className="flex gap-4 pb-3 mb-3 px-2 w-full overflow-x-scroll">
-        {crews.map((crew, index) => (
-          <CrewCard key={index} crew={crew} />
-        ))}
+        {props.crews &&
+          props.crews.map((crew, index) => (
+            <CrewCard key={index} crew={crew} />
+          ))}
       </div>
     </>
   );
@@ -73,20 +104,22 @@ const CrewSection = ({
 const CrewCard = ({ crew }: { crew: Crew }) => {
   return (
     <div className="bg-white rounded-lg p-4 shadow min-w-40 w-40 hover:scale-105 transition-transform">
-      <h3 className="text-sm font-semibold">{crew.name}</h3>
-      <p className="text-xl font-bold">{crew.value}</p>
-      <p className="text-sm text-gray-500">$365.51</p>
-      <div className="flex items-center mt-2">
-        <div className="flex-1 bg-purple-200 h-1 rounded-full relative">
-          <div
-            className="absolute left-0 top-0 h-1 bg-purple-500 rounded-full"
-            style={{ width: "75%" }}
-          ></div>
+      <Link to="/crew/$crewId" params={{ crewId: crew.crewName }}>
+        <h3 className="text-sm text-gray-400">{crew.investmentStyle}</h3>
+        <p className="text-xl font-bold">{crew.crewName}</p>
+        <p className="text-sm text-gray-500">{crew.crewYield}</p>
+        <div className="flex items-center mt-2">
+          <div className="flex-1 bg-purple-200 h-1 rounded-full relative">
+            <div
+              className="absolute left-0 top-0 h-1 bg-purple-500 rounded-full"
+              style={{ width: "75%" }}
+            ></div>
+          </div>
+          <span className="text-sm font-medium text-red-500 ml-2">
+            {crew.crewYield}
+          </span>
         </div>
-        <span className="text-sm font-medium text-red-500 ml-2">
-          {crew.change}
-        </span>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,125 +1,183 @@
+import { fetchAuthMe } from "@/api/auth";
+import { fetchBrokerAccounts } from "@/api/broker";
 import { MainLayout } from "@/components/MainLayout";
+import { BrokerAccount } from "@/types/api";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import React from "react";
 import { PieChart, Pie, Cell } from "recharts";
 
 export const UserPage = () => {
+  const results = useQueries({
+    queries: [
+      {
+        queryKey: ["broker"],
+        queryFn: fetchBrokerAccounts,
+        staleTime: Infinity,
+      },
+    ],
+  });
+
   return (
     <MainLayout>
       <UserProfile />
-      <Portfolio />
-      <MyStocks />
+      <Portfolio data={results[0].data} />
+      <MyStocks data={results[0].data} />
     </MainLayout>
   );
 };
 
-const UserProfile = () => (
-  <section className="bg-white p-4 rounded-lg shadow-sm mb-4 space-y-4">
-    <div className="flex items-center space-x-4">
-      <div className="w-12 h-12 border-2 border-gray-300 rounded-full flex items-center justify-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 19.5a9 9 0 0115 0"
-          />
-        </svg>
+const UserProfile = () => {
+  const result = useQuery({
+    queryKey: ["profile"],
+    queryFn: fetchAuthMe,
+    staleTime: Infinity,
+  });
+  return (
+    <section className="bg-white p-4 rounded-lg shadow-sm mb-4 space-y-4">
+      <div className="flex items-center space-x-4">
+        <div className="w-12 h-12 border-2 border-gray-300 rounded-full flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 19.5a9 9 0 0115 0"
+            />
+          </svg>
+        </div>
+        <div>
+          <h2 className="text-lg font-semibold">{result.data?.name}</h2>
+          <p className="text-sm text-gray-500">
+            {result.data?.investmentStyle}
+          </p>
+        </div>
       </div>
-      <div>
-        <h2 className="text-lg font-semibold">스티브 잡스</h2>
-        <p className="text-sm text-gray-500">빅테크 선호</p>
-      </div>
-    </div>
-    <button className="bg-gray-800 text-white py-2 px-4 rounded-md w-full">
-      로그아웃
-    </button>
-  </section>
-);
-
-const data = [
-  { name: "Group A", value: 400, stock: "엔비디아" },
-  { name: "Group B", value: 300, stock: "삼성" },
-  { name: "Group C", value: 300, stock: "인텔" },
-  { name: "Group D", value: 200, stock: "카카오" },
-  { name: "Group E", value: 100, stock: "기타" },
-];
+      <button className="bg-gray-800 text-white py-2 px-4 rounded-md w-full">
+        로그아웃
+      </button>
+    </section>
+  );
+};
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#CCC"];
 
-const Portfolio = () => (
-  <section className="bg-white p-4 rounded-lg shadow-sm mb-4">
-    <h3 className="text-lg font-semibold mb-4">포트폴리오</h3>
-    <div className="flex">
-      <div className="flex-1">
-        <div className="w-32 h-32 bg-gray-100 rounded-full flex items-center justify-center">
-          <PieChart width={128} height={128}>
-            <Pie
-              data={data}
-              cx="50%"
-              cy="50%"
-              innerRadius={48}
-              outerRadius={64}
-              fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-            >
-              {data.map((_, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                  className="outline-none"
-                />
-              ))}
-            </Pie>
-          </PieChart>
-          <span className="absolute text-sm font-semibold">투자비율</span>
+const Portfolio = React.memo(({ data }: { data?: BrokerAccount[] }) => {
+  const stockList = data
+    ? Object.values(
+        data
+          .map((broker) => broker.stocks)
+          .flat()
+          .reduce((prev: Record<string, typeof cur>, cur) => {
+            if (!prev[cur.stockName]) prev[cur.stockName] = { ...cur };
+            else {
+              prev[cur.stockName].evaluationAmount += cur.evaluationAmount;
+              prev[cur.stockName].investmentAmount += cur.investmentAmount;
+              prev[cur.stockName].quantity += cur.quantity;
+            }
+            return prev;
+          }, {})
+      )
+    : [];
+  const chartData = stockList.map((stock) => ({
+    name: stock.stockName,
+    value: stock.evaluationAmount,
+  }));
+  return (
+    <section className="bg-white p-4 rounded-lg shadow-sm mb-4">
+      <h3 className="text-lg font-semibold mb-4">포트폴리오</h3>
+      <div className="flex">
+        <div className="flex-1">
+          <div className="relative w-32 h-32 bg-gray-100 rounded-full flex items-center justify-center">
+            <PieChart width={128} height={128}>
+              <Pie
+                data={chartData}
+                cx="50%"
+                cy="50%"
+                innerRadius={48}
+                outerRadius={64}
+                fill="#8884d8"
+                paddingAngle={5}
+                dataKey="value"
+              >
+                {chartData.map((_, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                    className="outline-none"
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+            <span className="absolute text-sm font-semibold">투자비율</span>
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-1 text-gray-800">
+          {stockList.map((stock, i) => (
+            <div className="flex items-center text-sm" key={i}>
+              <div
+                style={{ background: COLORS[i % COLORS.length] }}
+                className="w-4 h-4 mr-2"
+              ></div>
+              {stock.stockName}
+            </div>
+          ))}
         </div>
       </div>
+      <div className="text-right text-red-500 font-bold text-lg">33.10%</div>
+    </section>
+  );
+});
 
-      <div className="flex-1 flex flex-col gap-1 text-gray-800">
-        {data.map((e, i) => (
-          <div className="flex items-center text-sm" key={i}>
-            <div
-              style={{ background: COLORS[i % COLORS.length] }}
-              className="w-4 h-4 mr-2"
-            ></div>
-            {e.stock}
-          </div>
-        ))}
-      </div>
-    </div>
-    <div className="text-right text-red-500 font-bold text-lg">33.10%</div>
-  </section>
-);
-
-const MyStocks = () => (
+const MyStocks = ({ data }: { data?: BrokerAccount[] }) => (
   <section>
     <h3 className="text-lg font-semibold mb-2">내 증권</h3>
     <div className="space-y-4">
-      <StockCard name="iM증권" amount="₩ 4,000,000" change="+40%" />
-      <StockCard name="삼성증권" amount="₩ 500,000" change="+5%" />
-      <StockCard name="토스증권" amount="₩ 2,000,000" change="+30%" />
+      {data &&
+        data.map((broker) => (
+          <StockCard
+            key={broker.brokerName}
+            name={broker.brokerName}
+            amount={broker.evaluationAmount}
+            change={
+              (100 * (broker.evaluationAmount - broker.investmentAmount)) /
+              broker.investmentAmount
+            }
+          />
+        ))}
     </div>
   </section>
 );
 
 type StockCardProps = {
   name: string;
-  amount: string;
-  change: string;
+  amount: number;
+  change: number;
 };
 
 const StockCard = ({ name, amount, change }: StockCardProps) => (
   <div className="flex justify-between items-center bg-white p-4 rounded-lg shadow-sm">
     <div>
       <p className="text-base font-semibold">{name}</p>
-      <p className="text-sm text-gray-500">{amount}</p>
+      <p className="text-sm text-gray-500">{amount}원</p>
     </div>
-    <div className="text-red-500 font-bold">{change}</div>
+    <div
+      className={
+        "font-bold " +
+        (change > 0
+          ? "text-red-500"
+          : change < 0
+            ? "text-blue-500"
+            : "text-gray-500")
+      }
+    >
+      {change}%
+    </div>
   </div>
 );
